f696de75ba9ed4dda861f21020b674867d4ccadd
Jordan Kite <jkite@consoloservices.com>
2017-10-12 10:05:20 -0400

    add update to portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,6 +19,24 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item Successfully Updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
+
+
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
   end
